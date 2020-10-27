2e097936532bd81958387343ff6733c2f05e1f03
purwowd <id.purwowd@gmail.com>
2020-10-27 07:55:34 +0700

    Fixed rename project command

--- src/django_project/apps/core/management/commands/renameproj.py
+++ src/django_project/apps/core/management/commands/renameproj.py
@@ -1,20 +1,22 @@
 import os
 
-from django.core.management import BaseCommand
+from django.core.management.base import BaseCommand
 from django.conf import settings
 
 
 class Command(BaseCommand):
+    def handle(self, *args, **options):
+        pass
 
     help = 'renames the project'
 
-
-    def handle(self, argv, *args, **kwargs):
-        self.stdout.write(self.style.WARNING('Start ... ...'))
+    def run_from_argv(self, argv):
+        self.stdout.write(self.style.WARNING('\nRenaming was processing ...'))
         old_project_name = os.path.basename(settings.BASE_DIR)
         if old_project_name == argv[2]:
             print('There is no change in the project name')
         else:
             os.rename(settings.BASE_DIR, os.path.join(os.path.join(os.path.dirname(settings.BASE_DIR), argv[2])))
-            print('Project name is changed from {} to {}'.format(old_project_name, argv[2]))
-            self.stdout.write(self.style.SUCCESS('Finished ... ...'))
+            info = 'Project name is changed from {} to {}'.format(old_project_name, argv[2])
+            self.stdout.write('=> ' + self.style.SUCCESS('Successfully, ') + info)
+            print('')
